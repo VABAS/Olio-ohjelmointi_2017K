8bebb547f0006fb5b0bc0ddd41128effcdecb296
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-01-18 19:54:54 +0200

    Yksinkertaistettu h02t05 valmiita metodeita käyttämällä

--- src/h02/h02t05.cs
+++ src/h02/h02t05.cs
@@ -3,20 +3,20 @@ class Program
 {
     static void Main()
     {
-        int[] arrayOne = {10,20,30,40,50};
-        int[] arrayTwo = {5,15,25,35,45};
+        int[] arrayOne = {10, 20, 30, 40, 50};
+        int[] arrayTwo = {5, 15, 25, 35, 45};
+
+        // Creating new (empty) array with size of arrays One and Two combined.
         int[] arrayThree = new int[arrayOne.Length + arrayTwo.Length];
-        
-        // Combining arrayOne and arrayTwo into arrayThree.
-        for (int i = 0; i < arrayOne.Length; i++)
-        {
-            arrayThree[i * 2 + 1] = arrayOne[i];
-            arrayThree[i * 2]  = arrayTwo[i];
-        }
-        
+
+        // Combining arrayOne and arrayTwo into arrayThree using arrays CopyTo
+        // -method.
+        arrayOne.CopyTo(arrayThree, 0);
+        arrayTwo.CopyTo(arrayThree, arrayOne.Length);
+
         // Sorting array using Sort-method from Array.
         Array.Sort(arrayThree);
-        
+
         // Printing sorted arrayThree. Using for-loop instead of foreach to
         // print also nice commas after each integer except last.
         for (int i = 0; i < arrayThree.Length; i++)
@@ -27,5 +27,6 @@ class Program
                 Console.Write(", ");
             }
         }
+        Console.WriteLine(".");
     }
 }
