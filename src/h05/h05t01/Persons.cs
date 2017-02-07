3c5ec58e7298ec63d444c4d4dfd5fd4965f9e3a7
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-02-07 13:01:28 +0200

    Lisätty namespace, H05T01

--- src/h05/h05t01/Persons.cs
+++ src/h05/h05t01/Persons.cs
@@ -1,74 +1,77 @@
 using System.Collections.Generic;
 
-class Persons
+namespace H05T01
 {
-    private List<Person> persons = new List<Person>();
-    
-    // Default constructor.
-    public Persons ()
+    class Persons
     {
+        private List<Person> persons = new List<Person>();
         
-    }
-    
-    // Paramerized constructor. List of persons to initially add into list.
-    public Persons (List<Person> persons)
-    {
-        this.persons = persons;
-    }
-    // Paramerized constructor. Array of persons to initially add into list.
-    public Persons (Person[] persons)
-    {
-        foreach (Person p in persons)
+        // Default constructor.
+        public Persons ()
         {
-            AddPerson(p);
+            
         }
-    }
-    
-    // Adds person object provided to persons list.
-    public void AddPerson (Person person)
-    {
-        persons.Add(person);
-    }
-    
-    // Adds new person object to array with provided id, firstName and lastName.
-    public void AddPerson (string id, string firstName, string lastName)
-    {
-        persons.Add(new Person(id, firstName, lastName));
-    }
-    
-    // Returns person with index number.
-    public Person GetPerson (int index)
-    {
-        try
+        
+        // Paramerized constructor. List of persons to initially add into list.
+        public Persons (List<Person> persons)
         {
-            return persons[index];
+            this.persons = persons;
         }
-        catch
+        // Paramerized constructor. Array of persons to initially add into list.
+        public Persons (Person[] persons)
         {
-            return null;
-        }
-    }
-    
-    // Method to find person with id. Returns person object found. Returns null
-    // if person could not be found with id provided.
-    public Person FindPerson (string id)
-    {
-        foreach (Person p in persons)
-        {
-            if (p.Id == id)
+            foreach (Person p in persons)
             {
-                return p;
+                AddPerson(p);
             }
         }
-        return null;
-    }
-    
-    // Prints information about every person object contained in persons list.
-    public void PrintData ()
-    {
-        foreach (Person p in persons)
+        
+        // Adds person object provided to persons list.
+        public void AddPerson (Person person)
         {
-            p.PrintData();
+            persons.Add(person);
+        }
+        
+        // Adds new person object to array with provided id, firstName and lastName.
+        public void AddPerson (string id, string firstName, string lastName)
+        {
+            persons.Add(new Person(id, firstName, lastName));
+        }
+        
+        // Returns person with index number.
+        public Person GetPerson (int index)
+        {
+            try
+            {
+                return persons[index];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        
+        // Method to find person with id. Returns person object found. Returns null
+        // if person could not be found with id provided.
+        public Person FindPerson (string id)
+        {
+            foreach (Person p in persons)
+            {
+                if (p.Id == id)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+        
+        // Prints information about every person object contained in persons list.
+        public void PrintData ()
+        {
+            foreach (Person p in persons)
+            {
+                p.PrintData();
+            }
         }
     }
 }
