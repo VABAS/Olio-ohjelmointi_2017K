12cacc8d7fb92c43ffa1229a235a25317f683dcc
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-02-17 10:51:58 +0200

    Lisätty h06t03

---
+++ src/h06/h06t03/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace H06T03
+{
+    public sealed partial class MainPage : Page
+    {
+        // Conversion rate as readonly double.
+        private readonly double conversionRate = 5.946;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Euro and markka conversions methods. Comma is replaced with period becouse of different decimal separators.
+        // Error handling done here also using double.TryParse().
+        private void toEuro(object sender, RoutedEventArgs e)
+        {
+            double result;
+            bool isOk = double.TryParse(textBoxMarkka.Text.Replace(",", "."), out result);
+            if (isOk)
+            {
+                textBoxEuro.Text = (result / conversionRate).ToString("0.00");
+            }
+            else
+            {
+                textBoxEuro.Text = "ERROR";
+            }
+        }
+
+        private void toMarkka(object sender, RoutedEventArgs e)
+        {
+            double result;
+            bool isOk = double.TryParse(textBoxEuro.Text.Replace(",", "."), out result);
+            if (isOk)
+            {
+                textBoxMarkka.Text = (result * conversionRate).ToString("0.00");
+            }
+            else
+            {
+                textBoxMarkka.Text = "ERROR";
+            }
+        }
+    }
+}
