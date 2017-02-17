f71e6bdc560fbede4937f927d172f38063ea7504
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-02-17 10:51:53 +0200

    Lisätty h06t02

---
+++ src/h06/h06t02/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace H06T02
+{
+    public class Car
+    {
+        private string name;
+        private int count;
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public Car (string name)
+        {
+            Name = name;
+            count = 0;
+        }
+
+        // Method which increments the car count and returns new value.
+        public int add ()
+        {
+            return ++count;
+        }
+
+        // Method for setting count to zero.
+        public void clear ()
+        {
+            count = 0;
+        }
+    }
+}
---
+++ src/h06/h06t02/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace H06T02
+{
+    public sealed partial class MainPage : Page
+    {
+        // Initializing objects to null.
+        private Car truck;
+        private Car car;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Creating instances of the car class.
+            truck = new Car("Sisu");
+            car = new Car("Audi");
+            
+            // Assigning button contents to represent the car-objects name-field.
+            trucksButton.Content = truck.Name;
+            carsButton.Content = car.Name;
+        }
+
+        // Incrementing truck count.
+        private void truckClick(object sender, RoutedEventArgs e)
+        {
+            trucksTextBlock.Text = truck.add().ToString();
+        }
+
+        // Incrementing car count.
+        private void carClick(object sender, RoutedEventArgs e)
+        {
+            carsTextBlock.Text = car.add().ToString();
+        }
+
+        // Clearing all clicks.
+        private void clearClick(object sender, RoutedEventArgs e)
+        {
+            trucksTextBlock.Text = "0";
+            carsTextBlock.Text = "0";
+            truck.clear();
+            car.clear();
+        }
+    }
+}
